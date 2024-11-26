f9bed3208a567eccff27175b9080cc2caab1b482
SheenaKiran <86108862+SheenaKiran@users.noreply.github.com>
2024-11-26 19:00:12 +0530

    Create GreatertThan10.py

---
+++ Hands-on Practice/Module 3/GreatertThan10.py
@@ -0,0 +1,9 @@
+# Write a Python program to read number and check whether it is greater then 10 or not
+num = int(input("Enter a number "))
+if num>10:
+    print(f"{num} is greater than 10")
+else:
+    print(f"{num} is not greater than 10")
+print('This statement is always executed')
+
+
