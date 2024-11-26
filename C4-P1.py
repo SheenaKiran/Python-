ad706fe2e90ff1f61ea167350c2ef39feff4fa53
SheenaKiran <86108862+SheenaKiran@users.noreply.github.com>
2024-11-26 18:33:15 +0530

    Add files via upload

---
+++ C4-P1.py
@@ -0,0 +1,6 @@
+'''Write a Python program to accept a user’s name and age, and display a message saying ”Hello
+< Name >, you are < Age > years old.'''
+
+name = input("Enter the name : ")
+age = int(input("Enter the age : "))
+print(f"Hello {name}, you are {age} years old")
